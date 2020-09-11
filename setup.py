9df6b243ee030b0cf33513fb5a8540bc00ebe9de
Maxime Bouroumeau-Fuseau <maxime.bouroumeau@gmail.com>
2020-09-11 17:26:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='jinja-layout',
-    version='0.1',
+    version='0.2',
     url='http://github.com/frascoweb/jinja-layout',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
